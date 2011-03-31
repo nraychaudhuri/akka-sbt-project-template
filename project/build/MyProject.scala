7826259ae66d0275f9bf513048fca8f174259176
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2011-03-30 22:51:05 -0400

    changed to specs2

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -13,5 +13,5 @@ class MyProject(info: ProjectInfo)
     val akkaSpring = akkaModule("spring")
     val akkaJta = akkaModule("jta")
     val scalaToolsSnapshots = "Scala Tools Snapshots" at  "http://scala-tools.org/repo-snapshots/"
-    val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.8-SNAPSHOT" % "test"
+    val specs = "org.specs2" % "specs2_2.8.1" % "1.1-SNAPSHOT" % "test"
 }
